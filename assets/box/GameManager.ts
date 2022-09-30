f89f7bffebcea577fb1963a47f1c8b5b184bae75
newyuanbin98 <yuanbinnew98@gmail.com>
2022-09-30 16:12:41 +0800

    show coin numbers using sprites

--- assets/box/GameManager.ts
+++ assets/box/GameManager.ts
@@ -4,7 +4,7 @@
 //  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
-import{Prefab, instantiate, Component } from 'cc';
+import{Prefab, instantiate, Component} from 'cc';
 import { boxScript } from './boxScript';
 import { GetResults } from './GetResults';
 const {ccclass, property} = cc._decorator;
@@ -13,6 +13,7 @@ const {ccclass, property} = cc._decorator;
 export class GameManager extends cc.Component {
     static instance: GameManager = null;
     
+    //Main_Panel stuff
     @property(cc.Label)
     label: cc.Label = null;
     @property
@@ -27,6 +28,17 @@ export class GameManager extends cc.Component {
     @property(cc.Node)
     public Boxes: Node[] = [];
     
+    //Result_Panel stuff
+    @property(cc.SpriteFrame)
+    public Numbers: SpriteFrame[] = [];
+    @property(cc.Node)
+    public coinNum_parent: Node;
+    @property(cc.Node)
+    public Coin_num: Node;
+    @property(cc.Node)
+    public Comma_node: Node;
+    @property()
+    public coin_distBtwn: number = 0;
     
     testJsonString: string;
     // LIFE-CYCLE CALLBACKS:
@@ -38,6 +50,7 @@ export class GameManager extends cc.Component {
     start () {
         this.ResultPanel.active = false;
         //this.label.string = this.TotalCoins;
+        
     }
     
     updateCoinText(coin: number){
@@ -47,7 +60,8 @@ export class GameManager extends cc.Component {
         
         this.MainPanel.active = false;
         this.ResultPanel.active = true;
-        
+
+        this.showResult();
         //cc.find("Canvas/CoinText").active = false;
     }
     
@@ -59,16 +73,55 @@ export class GameManager extends cc.Component {
     }
     
     showResult(){
-        
+        let randNum = cc.math.randomRangeInt(500,200000000);;
+        this.spawnNumberNode(randNum);
     }
     
     reset(){
         this.TotalCoins = 0;
         this.label.string = "";
+        
+        // for (var childNode = 0; childNode < this.coinNum_parent.childNodes.length; childNode++) {
+        //     this.coinNum_parent.childNodes.destroy();
+        // }
+        this.coinNum_parent.removeAllChildren();
+        
         this.MainPanel.active = true;
         this.ResultPanel.active = false;
     }
     
+    spawnNumberNode(totalCoins: number){
+        //let scene = cc.director.getScene();
+        
+        //console.log("randNum = "+totalCoins);
+        let parentNode:Node = this.coinNum_parent;
+        
+        let commaSpace = 0;
+        let commaCount = 0;
+        let totalSpace = 0;
+        for(let i = totalCoins.toString().length - 1; i >= 0; i--){
+            commaCount++;
+            if(commaCount==4||commaCount==7){//spawn comma node
+                this.Comma_node.getComponent(cc.Sprite).spriteFrame = this.Numbers[10];
+                let node = cc.instantiate(this.Comma_node);
+                node.parent = parentNode;
+                let Pos = -((totalCoins.toString().length-1-i)*this.coin_distBtwn)-commaSpace+10;
+                node.setPosition(Pos,-10,0);
+                commaSpace += 50;
+            }
+            
+            this.Coin_num.getComponent(cc.Sprite).spriteFrame = this.Numbers[totalCoins.toString().charAt(i)];
+            let node = cc.instantiate(this.Coin_num);
+            node.parent = parentNode;
+            let Pos = -((totalCoins.toString().length-1-i)*this.coin_distBtwn)-commaSpace;
+            node.setPosition(Pos,0,0);
+            
+            if(i==0)
+                totalSpace = -Pos;
+        }
+        
+        parentNode.setPosition(totalSpace/2,0,0);
+    }
     
     // update (dt) {}
 }
