97b6a05e939dea7b152b2d3e972f37a6dd456acd
newyuanbin98 <yuanbinnew98@gmail.com>
2022-10-28 14:40:38 +0800

    roulette build

--- assets/wheel/WheelManager.ts
+++ assets/wheel/WheelManager.ts
@@ -24,31 +24,49 @@ export default class WheelManager extends cc.Component {
     public sideStopNumber: number = 0;
 
     isSpinning:boolean = false;
+    
+    @property(cc.Node)
+    public MainPanel: Node = null;
+    @property(cc.Node)
+    public ResultPanel: Node = null;
+    
+    //Result_Panel stuff
+    @property(cc.SpriteFrame)
+    public Numbers: SpriteFrame[] = [];
+    @property(cc.Node)
+    public coinNum_parent: Node;
+    @property(cc.Node)
+    public Coin_num: Node;
+    @property(cc.Node)
+    public Comma_node: Node;
+    @property()
+    public coin_distBtwn: number = 0;
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
     start () {
-        this.isSpinning = false;
+        this.reset();
         //this.SpinWheel();
-        console.log(" Game start");
-        
-        
+        //console.log(" Game start");
     }
 
     // update (dt) {}
     
     SpinWheel(){
         
-        console.log("start spinning for "+this.spinDuration +" second(s)");
+        //console.log("start spinning for "+this.spinDuration +" second(s)");
         this.isSpinning = true;
         this.WheelObj.runAction(cc.rotateTo(this.spinDuration,this.stopSpinAngle(this.sideStopNumber)));
         setTimeout(()=> {
-            console.log("finish spinning");
+            //console.log("finish spinning");
             this.isSpinning = false;
+            this.MainPanel.active = true;
+            this.ResultPanel.active = true;
+            this.spawnNumberNode(this.getValueFromURL());
             console.log("spinning state after timeout = "+this.isSpinning);
-        },this.spinDuration*1000);
-        console.log("spinning state before timeout = "+this.isSpinning);
+        },this.spinDuration*1000+2000);
+       console.log("spinning state before timeout = "+this.isSpinning);
     }
     
     stopSpinAngle(sideToStopOn:number){
@@ -64,14 +82,64 @@ export default class WheelManager extends cc.Component {
         
         if(!this.isSpinning)
         {
-            console.log("going to start spinning");
-            
+            //console.log("going to start spinning");
             this.sideStopNumber = customEventData;
             this.SpinWheel();
         }
         else
-            console.log("spin not yet complete");
+            //console.log("spin not yet complete");
         
+    }
+    
+    spawnNumberNode(totalCoins: number){
+        //let scene = cc.director.getScene();
+        
+        //console.log("randNum = "+totalCoins);
+        let parentNode:Node = this.coinNum_parent;
+        
+        let commaSpace = 0;
+        let commaCount = 0;
+        let totalSpace = 0;
+        for(let i = totalCoins.toString().length - 1; i >= 0; i--){
+            commaCount++;
+            if(commaCount == 4 || commaCount == 7){//spawn comma node
+                this.Comma_node.getComponent(cc.Sprite).spriteFrame = this.Numbers[10];
+                let node = cc.instantiate(this.Comma_node);
+                node.parent = parentNode;
+                let Pos = -((totalCoins.toString().length-1-i)*this.coin_distBtwn)-commaSpace+15;
+                node.setPosition(Pos,-15,0);
+                commaSpace += 50;
+            }
+            
+            this.Coin_num.getComponent(cc.Sprite).spriteFrame = this.Numbers[totalCoins.toString().charAt(i)];
+            let node = cc.instantiate(this.Coin_num);
+            node.parent = parentNode;
+            let Pos = -((totalCoins.toString().length-1-i)*this.coin_distBtwn)-commaSpace;
+            node.setPosition(Pos,0,0);
+            
+            if(i==0)
+                totalSpace = -Pos;
+        }
+        
+        parentNode.setPosition(totalSpace/2, 0, 0);
+    }
+    
+    getValueFromURL()
+    {
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        var c = url.searchParams.get("v");
+        console.log("value v from url = "+c);
+        
+        return c;
+    }
+    
+    reset(){
+        this.isSpinning = false;
+        this.coinNum_parent.removeAllChildren();
+        
+        this.MainPanel.active = true;
+        this.ResultPanel.active = false;
     }
     // trySpin(obj: Node){
     //     cc.tween(obj)
